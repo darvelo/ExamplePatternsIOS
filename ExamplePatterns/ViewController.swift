ef94ce46327172089ce6b45debdc9b15d9c147f2
David Arvelo <david@davidarvelo.com>
2023-03-11 14:25:41 +0700

    Initial Commit

---
+++ ExamplePatterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamplePatterns
+//
+//  Created by David Arvelo on 3/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
